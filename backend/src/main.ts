60350b05a92c0e50cc75eda388848a4f8faa9fcd
Aya-we <ayamouhtad0@gmail.com>
2026-05-16 15:05:54 +0100

    Nuclear CORS fix: manual headers

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,18 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
+    
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200);
+    } else {
+      next();
+    }
+  });
+
   app.enableCors();
 
   // Serve static files from uploads directory
