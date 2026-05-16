d461d21738138bbb9d7deacb003ff45df800e10f
Aya-we <ayamouhtad0@gmail.com>
2026-05-16 14:56:29 +0100

    Simplify CORS to default

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,12 +6,7 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   
-  app.enableCors({
-    origin: ['https://pfe-edusmart-vcyv.vercel.app', 'http://localhost:3000'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type,Authorization',
-    credentials: true,
-  });
+  app.enableCors();
 
   // Serve static files from uploads directory
   app.useStaticAssets(join(process.cwd(), 'uploads'), {
